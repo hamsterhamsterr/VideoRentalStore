af64deca284719a58719160ac496ec9b95b0c92e
User Name Test <spirit2spiiirit9spirittt@gmail.com>
2023-04-28 15:52:37 +0300

    Add in customer section table with customers

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer
+                {
+                    Id = 1, Name = "John", LastName = "Smith"
+                },
+                new Customer
+                {
+                    Id = 2, Name = "Mary", LastName = "Williams"
+                }
+            };
+
+            var viewModel = new CustomersListViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
