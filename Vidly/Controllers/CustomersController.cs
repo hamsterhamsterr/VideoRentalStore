2663d386c6010d38226c0e00c81b87af2abddfac
User Name Test <spirit2spiiirit9spirittt@gmail.com>
2023-04-29 22:28:54 +0300

    Add DbContext to CustomersController and remove CustomersViewModel where have been hardcode data

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -10,9 +10,21 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
         public ActionResult Index()
         {
-            var viewModel = new CustomersViewModel();
+            var viewModel = _context.Customers;
 
             return View(viewModel);
         }
@@ -20,7 +32,8 @@ namespace Vidly.Controllers
         [Route("Customers/Details/{id}")]
         public ActionResult Customer(int id)
         {
-            var customer = new CustomersViewModel().GetCustomerById(id);
+            //var customer = new CustomersViewModel().GetCustomerById(id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
