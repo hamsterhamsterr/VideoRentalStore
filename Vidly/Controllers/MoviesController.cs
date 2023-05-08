8c9fa607cb8249933fda5dff5fc8acac7c8e2307
User Name Test <spirit2spiiirit9spirittt@gmail.com>
2023-05-08 14:39:46 +0300

    Add authorize attribute to the save action in movies controller

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -99,6 +99,7 @@ namespace Vidly.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public ActionResult Save(Movie movie)
         {
             if (!ModelState.IsValid)
