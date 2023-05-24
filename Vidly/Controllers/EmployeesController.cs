8324cf31c94e9a9abaf5a6894b7aeb5c2d8fd4c0
User Name Test <spirit2spiiirit9spirittt@gmail.com>
2023-05-24 15:52:10 +0300

    Create Employees section

---
+++ Vidly/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public EmployeesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            var users = _context.Users.ToList();
+
+            return View(users);
+        }
+    }
+}
