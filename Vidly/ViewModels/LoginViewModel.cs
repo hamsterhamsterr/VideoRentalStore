5cc3fbeab6fdda761aa7b4701f84b0e93ab850c6
User Name Test <spirit2spiiirit9spirittt@gmail.com>
2023-07-23 19:39:41 +0300

    Change style of login page

--- Vidly/ViewModels/LoginViewModel.cs
+++ Vidly/ViewModels/LoginViewModel.cs
@@ -9,16 +9,16 @@ namespace Vidly.ViewModels
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Адрес электронной почты")]
+        [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Пароль")]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Display(Name = "Запомнить меня")]
+        [Display(Name = "Remember me")]
         public bool RememberMe { get; set; }
     }
 }
